febfe4d63d84feab24cb0ad2ec5040987797a9c4
Andreson Tianth <andresontianth@gmail.com>
2018-09-21 17:05:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,16 @@
         var p =     "";  
         var doc =   app.activeDocument;
         var pages = doc.pages;  
-        var pages_group = 2; //parseInt( prompt("Quantidade de páginas agrupadas por pdf?", 2, "Grupo de páginas") ) ; 
+        var pages_group = parseInt( prompt("Quantidade de páginas agrupadas por pdf?", 2, "Grupo de páginas") ) ; 
         var num_pages_groups_rest = pages.length % pages_group; 
-        var num_pages_groups = num_pages_groups_rest > 0 ? parseInt( ( pages.length/pages_group ) + num_pages_groups_rest ) :  parseInt( pages.length/pages_group ) ; 
+        var num_pages_groups = parseInt( ( pages.length/pages_group ) ) ; 
         var is_print_output = true; 
         var l_page = 1;
+        var f = Folder.selectDialog("Escolha uma pasta para exportar"); 
         var prefs = app.pdfExportPreferences ; 
         for(i = 1; i <= pages.length ; i ++ ){               
             //var page = pages[i]; 
+            var cur_page = i;
             if( !( i == 1 ) ){   
                 p = ( (pages_group * i) - 1 ) + "-" + ( ((pages_group * i) - 2) + pages_group );
             }else{                 
@@ -19,7 +21,7 @@
             };
             if( !( p == "" ) && (  ( ((pages_group * i) - 2) + pages_group ) <= pages.length) ){
                 var page_name = pages.item( i ).name; 
-                var file = new File("~/Desktop/export/" + p + ".pdf"); 
+                var file = new File( f +"/"+ p + ".pdf"); 
                 
                 prefs.pageRange = p ;
                 
